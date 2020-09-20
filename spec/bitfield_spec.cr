56e7565f22a58f323ed8c4dee7b3449132584ddf
Matthew Berry <me@mattrb.com>
2020-09-20 00:04:56 -0700

    basic first-pass implementation, requires declared lengths

--- spec/bitfield_spec.cr
+++ spec/bitfield_spec.cr
@@ -1,9 +1,35 @@
 require "./spec_helper"
 
-describe Bitfield do
-  # TODO: Write tests
+class TestField < BitField(UInt8)
+  bool :bool_val, 0
+  num :top_four, 4, 4
+end
 
-  it "works" do
-    false.should eq(true)
+describe BitField do
+  it "gets whole value" do
+    bf = TestField.new 0xAF
+    bf.value.should eq 0xAF
+  end
+
+  it "gets first four" do
+    bf = TestField.new 0x9C
+    bf.top_four.should eq 0x9
+  end
+
+  it "sets first four" do
+    bf = TestField.new 0x34
+    bf.top_four = 0x5
+    bf.top_four.should eq 0x5
+  end
+
+  it "gets bool" do
+    bf = TestField.new 0xFE
+    bf.bool_val.should eq false
+  end
+
+  it "sets bool" do
+    bf = TestField.new 0xFE
+    bf.bool_val = true
+    bf.bool_val.should eq true
   end
 end
--- src/bitfield.cr
+++ src/bitfield.cr
@@ -1,6 +1,44 @@
-# TODO: Write documentation for `Bitfield`
-module Bitfield
-  VERSION = "0.1.0"
+class BitField(T)
+  property value : T
 
-  # TODO: Put your code here
+  def initialize(@value : T)
+    {% raise "Cannot create a BitField from a non-integer" unless T <= Int %}
+  end
+
+  macro num(name, start, size)
+    def {{name.id}} : T
+      get_val({{start}}, {{size}})
+    end
+
+    def {{name.id}}=(val : T) : Nil
+      set_val({{start}}, {{size}})
+    end
+  end
+
+  macro bool(name, start)
+    def {{name.id}} : Bool
+      get_val({{start}}, 1) > 0
+    end
+
+    def {{name.id}}=(val : Bool) : Nil
+      val = val ? 1 : 0
+      set_val({{start}}, 1)
+    end
+  end
+
+  macro get_val(start, size)
+    (@value >> {{start}} & mask({{size}}))
+  end
+
+  macro set_val(start, size)
+    (@value = @value & ~shifted_mask({{size}}, {{start}}) | (val & mask({{size}})) << {{start}})
+  end
+
+  macro mask(size)
+    ((1 << {{size}}) - 1)
+  end
+
+  macro shifted_mask(size, start)
+    (mask({{size}}) << {{start}})
+  end
 end

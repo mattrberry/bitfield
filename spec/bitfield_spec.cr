2faab42f0d6333a1bd8723dcc9d8a80455021579
Matthew Berry <me@mattrb.com>
2020-09-21 23:12:44 -0700

    test for runtime exceptions, simplify SIZE further

--- spec/bitfield_spec.cr
+++ spec/bitfield_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-class TestField < BitField(UInt8)
+class Test8 < BitField(UInt8)
   num four, 4
   bool bool
   num three, 3
@@ -14,52 +14,60 @@ class Test32 < BitField(UInt32)
   num rest, 28
 end
 
+class TestTooFew < BitField(UInt16)
+  num too_few, 15
+end
+
+class TestTooMany < BitField(UInt16)
+  num too_many, 17
+end
+
 describe BitField do
   it "gets whole value" do
-    bf = TestField.new 0xAF
+    bf = Test8.new 0xAF
     bf.value.should eq 0xAF
   end
 
   it "sets whole value" do
-    bf = TestField.new 0xAF
+    bf = Test8.new 0xAF
     bf.value = 0xFA
     bf.value.should eq 0xFA
   end
 
   it "gets upper four" do
-    bf = TestField.new 0x9C
+    bf = Test8.new 0x9C
     bf.four.should eq 0x9
     bf.value.should eq 0x9C
   end
 
   it "sets upper four" do
-    bf = TestField.new 0x9C
+    bf = Test8.new 0x9C
     bf.four = 0x5
     bf.four.should eq 0x5
     bf.value.should eq 0x5C
   end
 
   it "gets bool" do
-    bf = TestField.new 0xF7
+    bf = Test8.new 0xF7
     bf.bool.should eq false
     bf.value.should eq 0xF7
   end
 
   it "sets bool" do
-    bf = TestField.new 0xF7
+    bf = Test8.new 0xF7
     bf.bool = true
     bf.bool.should eq true
     bf.value.should eq 0xFF
   end
 
   it "gets lower three" do
-    bf = TestField.new 0x0F
+    bf = Test8.new 0x0F
     bf.three.should eq 0x07
     bf.value.should eq 0x0F
   end
 
   it "sets lower three" do
-    bf = TestField.new 0x0F
+    bf = Test8.new 0x0F
     bf.three = 0x5
     bf.three.should eq 0x5
     bf.value.should eq 0x0D
@@ -73,5 +81,17 @@ describe BitField do
     bf.four.should eq true
     bf.rest.should eq 0xF0000F5
     bf.value.should eq 0x5F0000F5
+  end
+
+  it "prints exception on too few" do
+    expect_raises(Exception, "You must describe exactly 16 bits (15 bits have been described)") do
+      bf = TestTooFew.new 0x0000
+    end
+  end
+
+  it "prints exception on too many" do
+    expect_raises(Exception, "You must describe exactly 16 bits (17 bits have been described)") do
+      bf = TestTooMany.new 0x0000
+    end
   end
 end
--- src/bitfield.cr
+++ src/bitfield.cr
@@ -2,18 +2,17 @@ abstract class BitField(T)
   macro inherited
     {% raise "Cannot create a BitField from a non-integer" unless T <= Int %}
 
-    SIZE = [0] # stored in an array so that it can be mutated at compile-time
-    FIELDS = [] of Tuple(String, Symbol, Int32) # name, type, size
-
-    property value : T
-
-    def initialize(@value : T)
-      bits = sizeof(T) * 8
-      raise "You must describe exactly #{bits} bits (#{SIZE[0]} bits have been described)" unless SIZE[0] == bits
-    end
+    FIELDS = [] of Tuple(String, Symbol, Int32) # name, type, size (types don't actually matter here..)
 
     macro finished
       build_methods
+
+      property value : T
+
+      def initialize(@value : T)
+        bits = sizeof(T) * 8
+        raise "You must describe exactly #{bits} bits (#{SIZE} bits have been described)" unless SIZE == bits
+      end
     end
   end
 
@@ -25,26 +24,10 @@ abstract class BitField(T)
     {% FIELDS << {name, :bool, 1} %}
   end
 
-  macro get_val(size, start)
-    (@value >> {{start}} & mask({{size}}))
-  end
-
-  macro set_val(size, start)
-    (@value = @value & ~shifted_mask({{size}}, {{start}}) | (val & mask({{size}})) << {{start}})
-  end
-
-  macro mask(size)
-    ((1 << {{size}}) - 1)
-  end
-
-  macro shifted_mask(size, start)
-    (mask({{size}}) << {{start}})
-  end
-
   macro build_methods
     {% pos = 0 %}
     {% FIELDS.map {|f| pos += f[2]} %}
-    {% SIZE[0] = pos %}
+    SIZE = {{pos}}
 
     {% for field in FIELDS %}
       {% name = field[0].id %}
@@ -63,5 +46,21 @@ abstract class BitField(T)
         set_val({{size}}, {{pos}})
       end
     {% end %}
+  end
+
+  macro get_val(size, start)
+    (@value >> {{start}} & mask({{size}}))
+  end
+
+  macro set_val(size, start)
+    (@value = @value & ~shifted_mask({{size}}, {{start}}) | (val & mask({{size}})) << {{start}})
+  end
+
+  macro mask(size)
+    ((1 << {{size}}) - 1)
+  end
+
+  macro shifted_mask(size, start)
+    (mask({{size}}) << {{start}})
   end
 end
